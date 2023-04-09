407bfa03ee05b12bcd5297ee4d3e30f73bf63cf1
Kelly Pellas <kkpellas@gmail.com>
2023-04-08 22:11:23 -0700

    fix(5-printf.c): fix traling whitespace error

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-
-/**
- * main - prints statement 
+/*
+ * main - prints statement
  * return: 0
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
