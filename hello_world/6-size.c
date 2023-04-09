3da98902a38da959c83a2e7f89779a124754f6e5
Kelly Pellas <kkpellas@gmail.com>
2023-04-08 22:21:15 -0700

    feat(6-size.c): add file

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+/*
+ * File: 6-size.c
+ * Auth: Kelly Pellas 
+ */
+
+#include <stdio.h>
+
+/**
+ *  main - Prints "with proper grammar, but the outcome a piece of art,"
+ * 
+ *  Return: 0
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
