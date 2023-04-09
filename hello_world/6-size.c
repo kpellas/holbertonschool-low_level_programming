37ee996874893a99464557ff89128f913ec855ab
Kelly Pellas <kkpellas@gmail.com>
2023-04-08 22:26:00 -0700

    fix(6-size.c): correct file

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,17 +1,21 @@
 /*
  * File: 6-size.c
- * Auth: Kelly Pellas 
+ * Auth: Kelly Pellas
  */
 
 #include <stdio.h>
 
 /**
- *  main - Prints "with proper grammar, but the outcome a piece of art,"
- * 
- *  Return: 0
+ * main - Prints the size of various types based on the computer on which is compiled and run
+ * return: 0 
  */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
 }
