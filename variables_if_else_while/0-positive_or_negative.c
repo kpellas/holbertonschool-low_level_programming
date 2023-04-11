711186c2e3a8ec89c8d9c0b3362b8e0be2209d04
Kelly Pellas <kkpellas@gmail.com>
2023-04-11 01:03:10 -0700

    doc+fix(readme, 0-positive_or_negative): add documentation and correct script to add zero result

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -16,9 +16,13 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive", &n)
+	} else if (n < 0)
+	{
+
+		printf("%d is negative", &n)
 	} else
 	{
-		printf("%d is negative", &n)
+		printf("%d is zero", &n)
 	}
 	return (0);
 }
