47bcce5dff0c849f172733cfc1125b62960f1eb4
Kelly Pellas <kkpellas@gmail.com>
2023-04-11 15:49:24 -0700

    feat(6-print_numberz.c): add file

---
+++ variables_if_else_while/6-print_sumberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers from 0-9.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i = '0';
+
+	while (i < '10')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
