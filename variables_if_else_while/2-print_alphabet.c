ff4915c87c561c517296a74e70303c459850c13a
Kelly Pellas <kkpellas@gmail.com>
2023-04-11 02:20:45 -0700

    feat(2-print_alphabet): add file

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lower case
+ * Return: Always 0/
+*/
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	return (0);
+}
