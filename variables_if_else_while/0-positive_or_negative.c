a8615f8b779bd699f1d3ea9aeaa26f16c26ccc5d
Kelly Pellas <kkpellas@gmail.com>
2023-04-11 01:08:49 -0700

    fix(0-positive_or_negative.c): fix syntax

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
 /**
  * main - Prints whether a random number is
@@ -13,16 +13,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive", &n)
-	} else if (n < 0)
-	{
 
-		printf("%d is negative", &n)
-	} else
-	{
-		printf("%d is zero", &n)
-	}
+	if (n > 0)
+		printf("%d is positive\n", &n)
+	else if (n < 0)
+		printf("%d is negative\n", &n)
+	else
+		printf("%d is zero\n", &n)
 	return (0);
 }
