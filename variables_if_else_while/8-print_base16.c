6dbb003c23d93c9034d98a94a6bcac03767d0896
Kelly Pellas <kkpellas@gmail.com>
2023-04-11 22:10:37 -0700

    feat(8-print_base16.c): add file

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Prints numbesr of base 16
+*
+* Return: 0 on success
+*/
+int main(void)
+{
+
+	char ch = 0x00;
+	
+	while (ch <= 0xff){
+	}
+		putchar(ch + 01);
+		ch++;
+	putchar('\n');
+	return (0);
+}
